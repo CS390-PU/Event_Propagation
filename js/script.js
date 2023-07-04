5da096aa13c9b17c13cec6a690c886fc2fc8e132
ryan2135 <ryan2135@pacificu.edu>
2023-07-04 15:14:39 -0700

    Updated js event handler

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-console.log('Hello!');
+const list=document.querySelector('.list');
+
+list.addEventListener('click', e=> {
+  console.log(e.target);
+  console.log(e.currentTarget);
+  e.target.classList.toggle('completed');
+})
